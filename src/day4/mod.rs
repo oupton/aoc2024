649835f3c0287be7435e651056eacb1188f3932a
Oliver Upton <oliver.upton@linux.dev>
2024-12-04 00:37:30 -0800

    day 4 part 2
    NVM, did it anyway.
    
    Signed-off-by: Oliver Upton <oliver.upton@linux.dev>

--- src/day4/mod.rs
+++ src/day4/mod.rs
@@ -59,6 +59,37 @@ pub fn part1(input: &str) -> isize {
     count
 }
 
+fn check_cross(grid: &[Vec<char>], x: usize, y: usize) -> Option<isize> {
+    let mut count = 0;
+
+    count += check_direction(grid, "MAS", x.checked_sub(1)?, 1, y.checked_sub(1)?, 1).unwrap_or(0);
+    count += check_direction(grid, "MAS", x + 1, -1, y + 1, -1).unwrap_or(0);
+    count += check_direction(grid, "MAS", x.checked_sub(1)?, 1, y + 1, -1).unwrap_or(0);
+    count += check_direction(grid, "MAS", x + 1, -1, y.checked_sub(1)?, 1).unwrap_or(0);
+
+    if count == 2 {
+        Some(1)
+    } else {
+        None
+    }
+}
+
+pub fn part2(input: &str) -> isize {
+    let grid = input
+        .lines()
+        .map(|l| l.chars().collect::<Vec<_>>())
+        .collect::<Vec<Vec<_>>>();
+    let mut count = 0;
+
+    for y in 0..grid.len() {
+        for x in 0..grid[y].len() {
+            count += check_cross(&grid, x, y).unwrap_or(0);
+        }
+    }
+
+    count
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -74,5 +105,15 @@ mod tests {
     #[test]
     fn part1_test() {
         assert_eq!(part1(TEST), 2521)
+    }
+
+    #[test]
+    fn part2_example() {
+        assert_eq!(part2(EXAMPLE), 9);
+    }
+
+    #[test]
+    fn part2_test() {
+        assert_eq!(part2(TEST), 1912);
     }
 }
